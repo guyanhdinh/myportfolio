e8437247d002adec474657bbbb45d9c59f4c809c
Guy Dinh <110943645+guyanhdinh@users.noreply.github.com>
2023-03-18 01:18:34 -0500

    Add all files

---
+++ index.js
@@ -0,0 +1,16 @@
+var i = 0;
+var tag = document.getElementById("text");
+var html = document.getElementById("text").innerHTML;
+var attr = tag.setAttribute("data", html);
+var txt = tag.getAttribute("data");
+var speed = 70;
+
+function typeWriter() {
+    if (i <= txt.length) {
+      document.getElementById("text").innerHTML = txt.slice(0 , i + 1);
+      i++;
+      setTimeout(typeWriter, speed);
+    }
+  }
+
+typeWriter();
